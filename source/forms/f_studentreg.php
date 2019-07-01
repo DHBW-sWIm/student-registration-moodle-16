83f541dc59822a35f00e0b5bef77f3e7997d3602
Se Fu <basularasa@hotmail.com>
2019-07-01 18:37:28 +0200

    studentreg

---
+++ source/forms/f_studentreg.php
@@ -0,0 +1,55 @@
+<?php
+require_once("$CFG->libdir/formslib.php");
+
+class fstudentreg extends moodleform {
+    //Add elements to form
+    public function definition() {
+        global $CFG;
+        global $USER;
+
+        $mform = $this->_form; // Don't forget the underscore!
+
+        $mform->addElement('header', 'header', 'Enter a new student:');
+
+        $mform->addElement('text', 'firstname', 'First name:');
+        $mform->setType('firstname', PARAM_TEXT);
+        $mform->addRule('firstname', 'Please enter a first name', 'required');
+
+        $mform->addElement('text', 'surname', 'Surname:');
+        $mform->setType('surname', PARAM_TEXT);
+        $mform->addRule('surname', 'Please enter a surname', 'required');
+
+        $mform->addElement('text', 'emailaddress', 'Email address:');
+        $mform->setType('emailaddress', PARAM_TEXT);
+        $mform->addRule('emailaddress', 'Please enter a valid email address', 'email');
+
+        $mform->addElement('date_selector', 'birthdate', 'Birth date:', array(
+            'startyear' => 1990,
+            'stopyear'  => 2002,
+            'optional'  => false
+        ));
+        $mform->setType('birthdate', PARAM_TEXT);
+        $mform->addRule('birthdate', 'Please enter a valid birthdate', 'rule type', 'nonzero','client', false, false);
+
+        $mform->addElement('text', 'course', 'Course:');
+        $mform->setType('course', PARAM_TEXT);
+        $mform->addRule('course', 'Please enter a course', 'required');
+
+
+        $mform->addElement('text', 'company', 'Company:');
+        $mform->setType('company', PARAM_TEXT);
+        $mform->addRule('company', 'Please enter a company', 'required');
+
+
+        $mform->addElement('submit', 'btnSubmit', 'Submit');
+
+        $mform->addElement('hidden', 'id');
+        $mform->setType('id', PARAM_TEXT);
+
+    }
+
+    //Custom validation should be added here
+    function validation($data, $files) {
+        return array();
+    }
+}
---
+++ source/v_studentreg.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once(dirname(dirname(__DIR__)) . '/config.php');
+require_once(__DIR__ . '/lib.php');
+require_once(__DIR__ . '/locallib.php');
+
+include(__DIR__ . '/view_init.php');
+
+global $SESSION;
+
+echo $OUTPUT->heading('Student Registration Prototype');
+
+// Implement form for user
+// v_studentreg.php gets fstudentreg class which extends moodleform
+
+require_once(__DIR__ . '/forms/f_studentreg.php');
+
+$mform = new fstudentreg();
+
+$mform->render();
+
+
+$tablename = 'students';
+$records = $DB->get_records_select($tablename,  $params=null);
+$table_all_records = new html_table();
+$table_all_records->head = array('First Name', 'Surname', 'Email Address', 'Date of Birth', 'Course', 'Company');
+
+// bind data to html table
+foreach ($records as $record) {
+    $firstname = $record->firstname;
+    $surname = $record->surname;
+    $email = $record->email;
+    $birthdate = gmdate("d-m-Y", $record->birthdate);
+    $course = $record->course;
+    $company = $record->company;
+    $table_all_records->data[] = array($firstname, $surname, $email, $birthdate, $course, $company);
+}
+
+// print table
+echo html_writer::table($table_all_records);
+
+
+//Form processing and displaying is done here
+if ($mform->is_cancelled()) {
+    //Handle form cancel operation, if cancel button is present on form
+} else if ($fromform = $mform->get_data()) {
+    //Handle form successful operation, if button is present on form
+    $SESSION->formdata = $fromform;
+
+    $record = new stdClass();
+    $record->firstname = $fromform->firstname;
+    $record->surname = $fromform->surname;
+    $record->email = $fromform->emailaddress;
+    $record->birthdate = $fromform->birthdate;
+    $record->course = $fromform->course;
+    $record->company = $fromform->company;
+
+    $lastinsertid = $DB->insert_record('students', $record, false);
+    $returnurl = new moodle_url('/mod/studreg/v_studentreg.php', array('id' => $cm->id));
+    redirect($returnurl);
+
+} else {
+    // this branch is executed if the form is submitted but the data doesn't validate and the form should be redisplayed
+    // or on the first display of the form.
+
+    // Set default data (if any)
+    // Required for module not to crash as a course id is always needed
+    $formdata = array('id' => $id);
+    $mform->set_data($formdata);
+    //displays the form
+    $mform->display();
+}
+echo $OUTPUT->footer();
